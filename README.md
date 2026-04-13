9dafd3c03749791a13e8f55c109b668b70e2ee98
Ahmed Said <ahmedsaiedkhalaf2004@gmail.com>
2026-04-13 20:45:16 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# 🎓 Student Grade Calculator
+
+A simple web application to calculate students' total marks and display the final grade.
+
+## 🚀 Features
+
+* Calculate total marks out of 500
+* Show final grade (Excellent, Very Good, Good, Pass, Poor)
+* Clean and simple user interface
+
+## 🛠️ Built With
+
+* HTML
+* CSS
+* JavaScript
+
+## 📌 How to Use
+
+1. Enter marks for all subjects
+2. Click on **"Calculator Marks"**
+3. View total marks and grade
+
+## 📷 Preview
+
+(Add a screenshot here)
+
+## 💡 Notes
+
+This project is created for practicing JavaScript basics like:
+
+* DOM manipulation
+* Events
+* Conditions
---
+++ main.js
@@ -0,0 +1,30 @@
+let button = document.getElementById("btn");
+let result = document.getElementById("result");
+let Grade = document.getElementById("Grade")
+
+button.addEventListener("click", function () {
+  let physics = Number(document.getElementById("Physics").value);
+  let chemistry = Number(document.getElementById("Chemistry").value);
+  let maths = Number(document.getElementById("Maths").value);
+  let biology = Number(document.getElementById("Biology").value);
+  let total = physics + chemistry + maths + biology;
+  let gradeText = getGrade(total);
+  result.innerText = `The Mark Is = ${total} / 500 `;
+  Grade.innerHTML = gradeText
+});
+
+function getGrade(total) {
+  if (total >= 450) {
+    return "Excellent";
+  } else if (total >= 400) {
+    return "Very Good";
+  } else if (total >= 350) {
+    return "Good";
+  } else if (total >= 300) {
+    return "Pass";
+  } else if (total >= 250) {
+    return "Poor";
+  } else if (Input.value == "") {
+    return null;
+  }
+}
